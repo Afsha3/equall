e88f97d923de847eba043d7bf24a65e270e9dbe8
ADMIN <ADMIN@192.168.0.3>
2020-07-29 13:16:01 +0530

    equallignore

---
+++ stream/src/stream/Equalignorecaseexample.java
@@ -0,0 +1,12 @@
+package stream;
+
+public class Equalignorecaseexample {
+	public static void main(String args[]){ 
+		String s1="hi";
+		String s2="hello"; 
+		String s3="hi";
+		System.out.println(s1.equalsIgnoreCase(s2));   
+		System.out.println(s1.equalsIgnoreCase(s3));   
+		}}
+
+
